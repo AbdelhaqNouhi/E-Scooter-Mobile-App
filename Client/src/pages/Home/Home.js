3a0536b0e1ef5d40167ec2cc656c1b7f4cd42d02
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2023-02-25 14:53:19 +0100

    added mapbox

--- Client/src/pages/Home/Home.js
+++ Client/src/pages/Home/Home.js
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapboxGL from '@rnmapbox/maps';
+
+MapboxGL.setAccessToken('<pk.eyJ1IjoiYW5vdWhpIiwiYSI6ImNsZWJoeHJyODA4Z3kzbnJ4amZnaWV0NWEifQ.qeqMavoHxDNOHPfhrSCECg>');
 
 const Home = () => {
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.page}>
+            <View style={styles.container}>
+                <MapboxGL.MapView style={styles.map} />
+            </View>
         </View>
-    )
+    );
 }
 
-export default Home
+export default Home;
+
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    container: {
+        height: 300,
+        width: 300,
+    },
+    map: {
+        flex: 1
+    }
+});
